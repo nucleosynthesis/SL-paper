a570b4af15f69e32866804260cf3c8345ed0cd84
Nicholas Wardle <Nick@dyn-10-32-88-76.winet.local>
2018-09-04 09:42:35 +0100

    more fixes

--- HepData/convertPythonToYaml.py
+++ HepData/convertPythonToYaml.py
@@ -44,7 +44,7 @@ for x in model.background:
 
 f1.write("independent_variables:\n")
 f1.write("- header:\n")
-f1.write("    name: $m_3$\n")
+f1.write("    name: Bin Number\n")
 f1.write("  values:\n")
 
 for x in range(nb): 
@@ -84,7 +84,7 @@ f1 = open("Table_4.yaml","w")
 
 f1.write("dependent_variables:\n")
 f1.write("- header:\n")
-f1.write("    name: $m_1$\n")
+f1.write("    name: $m_3$\n")
 f1.write("  values:\n")
 
 for x in model.third_moment: 
@@ -92,7 +92,7 @@ for x in model.third_moment:
 
 f1.write("independent_variables:\n")
 f1.write("- header:\n")
-f1.write("    name: $m_3$\n")
+f1.write("    name: Bin Number\n")
 f1.write("  values:\n")
 
 for x in range(nb): 
