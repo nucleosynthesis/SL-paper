b0a6087c25d893775b15a1e3516cd21945fb9dfc
Nicholas Wardle <Nick@dyn-10-32-88-76.winet.local>
2018-09-04 09:38:43 +0100

    Updating with sepaarate tables

--- HepData/convertPythonToYaml.py
+++ HepData/convertPythonToYaml.py
@@ -30,7 +30,7 @@ for x in range(nb):
 
 f1.close()
 
-# now do the table for m_1 and m_3
+# now do the table for m_1
 
 f1 = open("Table_2.yaml","w")
 
@@ -42,17 +42,11 @@ f1.write("  values:\n")
 for x in model.background: 
   f1.write("  - value: %g\n"%x)
 
+f1.write("independent_variables:\n")
 f1.write("- header:\n")
 f1.write("    name: $m_3$\n")
 f1.write("  values:\n")
 
-for x in model.third_moment: 
-  f1.write("    - value: %g\n"%x)
-
-f1.write("independent_variables:\n")
-f1.write("- header:\n")
-f1.write("    name: Bin Number\n")
-f1.write("  values:\n")
 for x in range(nb): 
   f1.write("  - value: %d\n"%(x+1))
 
@@ -81,3 +75,28 @@ for x in range(nb):
   f1.write("  - value: %d\n"%(x+1))
 
 f1.close()
+
+
+
+# and a table for m3
+
+f1 = open("Table_4.yaml","w")
+
+f1.write("dependent_variables:\n")
+f1.write("- header:\n")
+f1.write("    name: $m_1$\n")
+f1.write("  values:\n")
+
+for x in model.third_moment: 
+  f1.write("  - value: %g\n"%x)
+
+f1.write("independent_variables:\n")
+f1.write("- header:\n")
+f1.write("    name: $m_3$\n")
+f1.write("  values:\n")
+
+for x in range(nb): 
+  f1.write("  - value: %d\n"%(x+1))
+
+f1.close()
+
